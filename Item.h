578d8e0b895749b0ed9b1e7694639752e5c33dec
IT21288562 <99407898+IT21288562@users.noreply.github.com>
2022-05-17 19:44:29 +0000

    completed this tutorial

--- Item.h
+++ Item.h
@@ -1,17 +1,18 @@
 // Item.h
 
-class Item {
-   private:
-      int itemCode;
-      float unitPrice;
-      float discount; // out of 100 e.g. discount = 15
-   public:
-      // 1. Write the Default Constructor
-      // 2. Write the Overloaded Constructor
-      // 3. Write the Destructor
+class Item
+{
+private:
+int itemCode;
+float unitPrice;
+float discount; // out of 100 e.g. discount = 15
 
-      void setDiscount(float punitPrice);
-      float getDiscount();
-      float discountedPrice();
-      void display();
+public:
+Item();
+Item(int itCode, float unPrice);
+~Item();
+void setDiscount(float punitPrice);
+float getDiscount();
+float discountedPrice();
+void display();
 };
